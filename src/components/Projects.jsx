21dd06c5da119b6778508a11d04b3b444fc78d53
jasvinderkhera <ankitkhera15@gmail.com>
2024-10-06 11:36:06 +0530

    latest project first

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,75 +3,141 @@ import music from "./images/music-player.png";
 import eduford from "./images/eduford.png";
 import udemy from "./images/udemy.png";
 import zaydn from "./images/zaydn.png";
-import flatmate from "./images/flatmate.png"
-import legal from "./images/legal.png"
-import crud_operations from "./images/crud_operations.png"
+import flatmate from "./images/flatmate.png";
+import legal from "./images/legal.png";
+import crud_operations from "./images/crud_operations.png";
 
 function Projects() {
   return (
     <div>
       <div className="row project_row">
-        <a href="https://musicplayer-ri2xbx4j6-jasvinderkheras-projects.vercel.app/" target="blank" className="col-4 mb-4 project_div nav-link">
+        <a
+          href="https://basic-crud-frontend-eight.vercel.app/"
+          target="blank"
+          className="col-4 mb-4 project_div nav-link"
+        >
           <div className="box border border-2">
-            <img src={music} alt="" className="img-fluid project_img" />
-            <a href="https://musicplayer-ri2xbx4j6-jasvinderkheras-projects.vercel.app/" target="blank" className="nav-link bg-secondary text-white">
-              Music Player
+            <img
+              src={crud_operations}
+              alt=""
+              className="img-fluid project_img text-center"
+            />
+            <a
+              href="https://basic-crud-frontend-eight.vercel.app/"
+              target="blank"
+              className="nav-link bg-secondary text-white"
+            >
+              CRUD Operations
             </a>
           </div>
         </a>
-        <a href="https://jasvinderkhera.github.io/eduford_university/" target="blank" className="col-4 mb-4 project_div nav-link">
-        <div className="box border border-2">
-            <img src={eduford} alt="" className="img-fluid project_img" />
-            <a href="https://jasvinderkhera.github.io/eduford_university/" target="blank" className="nav-link bg-secondary text-white">
-              EduFord University
-            </a>
-          </div>
-        </a>
-        <a href="https://jasvinderkhera.github.io/udemy_clone/" target="blank" className="col-4 mb-4 project_div nav-link">
-        <div className="box border border-2">
-            <img src={udemy} alt="" className="img-fluid project_img" />
-            <a href="https://jasvinderkhera.github.io/udemy_clone/" target="blank" className="nav-link bg-secondary text-white">
-              Udemy
-            </a>
-          </div>
-        </a>
-        <a href="https://jasvinderkhera.github.io/portfolio/zaydn/index.html" target="blank" className="col-4 mb-4 project_div nav-link">
-        <div className="box border border-2">
-            <img src={zaydn} alt="" className="img-fluid project_img text-center" />
-            <a href="https://jasvinderkhera.github.io/portfolio/zaydn/index.html" target="blank" className="nav-link bg-secondary text-white">
-              Zaydn
-            </a>
-          </div>
-        </a>
-        <a href="https://jasvinderkhera.github.io/flatmate/index.html" target="blank" className="col-4 mb-4 project_div nav-link">
-        <div className="box border border-2">
-            <img src={flatmate} alt="" className="img-fluid project_img text-center" />
-            <a href="https://jasvinderkhera.github.io/flatmate/index.html" target="blank" className="nav-link bg-secondary text-white">
-              FlatMate
-            </a>
-          </div>
-        </a>
-        <a href="https://flatmatefrontend.vercel.app/" target="blank" className="col-4 mb-4 project_div nav-link">
-        <div className="box border border-2">
-            <img src={flatmate} alt="" className="img-fluid project_img text-center" />
-            <a href="https://flatmatefrontend.vercel.app/" target="blank" className="nav-link bg-secondary text-white">
-              FlatMate-React
-            </a>
-          </div>
-        </a>
-        <a href="https://legal-so.vercel.app/" target="blank" className="col-4 mb-4 project_div nav-link">
-        <div className="box border border-2">
-            <img src={legal} alt="" className="img-fluid project_img text-center" />
-            <a href="https://legal-so.vercel.app/" target="blank" className="nav-link bg-secondary text-white">
+        <a
+          href="https://legal-so.vercel.app/"
+          target="blank"
+          className="col-4 mb-4 project_div nav-link"
+        >
+          <div className="box border border-2">
+            <img
+              src={legal}
+              alt=""
+              className="img-fluid project_img text-center"
+            />
+            <a
+              href="https://legal-so.vercel.app/"
+              target="blank"
+              className="nav-link bg-secondary text-white"
+            >
               Legal.So
             </a>
           </div>
         </a>
-        <a href="https://basic-crud-frontend-eight.vercel.app/" target="blank" className="col-4 mb-4 project_div nav-link">
-        <div className="box border border-2">
-            <img src={crud_operations} alt="" className="img-fluid project_img text-center" />
-            <a href="https://basic-crud-frontend-eight.vercel.app/" target="blank" className="nav-link bg-secondary text-white">
-              CRUD Operations
+        <a
+          href="https://jasvinderkhera.github.io/udemy_clone/"
+          target="blank"
+          className="col-4 mb-4 project_div nav-link"
+        >
+          <div className="box border border-2">
+            <img src={udemy} alt="" className="img-fluid project_img" />
+            <a
+              href="https://jasvinderkhera.github.io/udemy_clone/"
+              target="blank"
+              className="nav-link bg-secondary text-white"
+            >
+              Udemy
+            </a>
+          </div>
+        </a>
+        <a
+          href="https://flatmatefrontend.vercel.app/"
+          target="blank"
+          className="col-4 mb-4 project_div nav-link"
+        >
+          <div className="box border border-2">
+            <img
+              src={flatmate}
+              alt=""
+              className="img-fluid project_img text-center"
+            />
+            <a
+              href="https://flatmatefrontend.vercel.app/"
+              target="blank"
+              className="nav-link bg-secondary text-white"
+            >
+              FlatMate
+            </a>
+          </div>
+        </a>
+        <a
+          href="https://jasvinderkhera.github.io/eduford_university/"
+          target="blank"
+          className="col-4 mb-4 project_div nav-link"
+        >
+          <div className="box border border-2">
+            <img src={eduford} alt="" className="img-fluid project_img" />
+            <a
+              href="https://jasvinderkhera.github.io/eduford_university/"
+              target="blank"
+              className="nav-link bg-secondary text-white"
+            >
+              EduFord University
+            </a>
+          </div>
+        </a>
+
+        <a
+          href="https://musicplayer-ri2xbx4j6-jasvinderkheras-projects.vercel.app/"
+          target="blank"
+          className="col-4 mb-4 project_div nav-link"
+        >
+          <div className="box border border-2">
+            <img src={music} alt="" className="img-fluid project_img" />
+            <a
+              href="https://musicplayer-ri2xbx4j6-jasvinderkheras-projects.vercel.app/"
+              target="blank"
+              className="nav-link bg-secondary text-white"
+            >
+              Music Player
+            </a>
+          </div>
+        </a>
+
+        <a
+          href="https://jasvinderkhera.github.io/portfolio/zaydn/index.html"
+          target="blank"
+          className="col-4 mb-4 project_div nav-link"
+        >
+          <div className="box border border-2">
+            <img
+              src={zaydn}
+              alt=""
+              className="img-fluid project_img text-center"
+            />
+            <a
+              href="https://jasvinderkhera.github.io/portfolio/zaydn/index.html"
+              target="blank"
+              className="nav-link bg-secondary text-white"
+            >
+              Zaydn
             </a>
           </div>
         </a>
